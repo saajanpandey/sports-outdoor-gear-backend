17b7c6ce903baab6fec2e9822e8f0aea88bd7a35
Saajan Raj Pandey <saajan@Saajans-MacBook-Pro.local>
2025-07-21 23:40:50 -0400

    added migrations for admin user

---
+++ migrations/20250722033704-add_admin_collection.js
@@ -0,0 +1,38 @@
+module.exports = {
+  /**
+   * @param db {import('mongodb').Db}
+   * @param client {import('mongodb').MongoClient}
+   * @returns {Promise<void>}
+   */
+  async up(db, client) {
+    // TODO write your migration here.
+    // See https://github.com/seppevs/migrate-mongo/#creating-a-new-migration-script
+    // Example:
+    // await db.collection('albums').updateOne({artist: 'The Beatles'}, {$set: {blacklisted: true}});
+
+    await db.collection("users").insertOne({
+      first_name: "Admin",
+      last_name: "User",
+      email: "admin@admin.com",
+      password: "admin@123",
+      address: "Address",
+      phone: 1111111111,
+      role: 1,
+    });
+  },
+
+  /**
+   * @param db {import('mongodb').Db}
+   * @param client {import('mongodb').MongoClient}
+   * @returns {Promise<void>}
+   */
+  async down(db, client) {
+    // TODO write the statements to rollback your migration (if possible)
+    // Example:
+    // await db.collection('albums').updateOne({artist: 'The Beatles'}, {$set: {blacklisted: false}});
+
+    await db
+      .collection("users")
+      .deleteOne({ first_name: "Admin", last_name: "User" });
+  },
+};
